4dcebfdf856fa1ae598c651d1fd395e2d3cdd3d5
Bhuvaneshwaran <bhuvanshwaran@twinkletech.com>
2025-12-19 16:04:45 +0530

    new api added

--- deployServer.js
+++ deployServer.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("this api message is from deploy docker server.js");
 });
 
+app.get("/api/android", (req, res) => {
+  res.send("this message is from android in docker deploy");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
